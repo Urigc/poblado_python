96e139a0fe6a2bb1ecade025c5382700616eac62
Uriel Gonzalez <gonzalezcasianouriel@gmail.com>
2026-05-18 23:00:26 -0600

    Delete Practica 5 directory

--- Practica 5/scripts/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-USER = os.getenv("DB_USER", "urigc")
-PASS = os.getenv("DB_PASSWORD", "123456")
-HOST = os.getenv("DB_HOST", "localhost")
-PORT = os.getenv("DB_PORT", "5433")
-DB_NAME = os.getenv("DB_NAME", "Obras_Publicas")  
-
-DATABASE_URL = f"postgresql://{USER}:{PASS}@{HOST}:{PORT}/{DB_NAME}"
